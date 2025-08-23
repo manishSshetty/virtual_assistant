43e3887c6ef4307346e591640e80697602470e73
Manish Shetty <manishshetty827@gmail.com>
2025-08-23 16:37:42 +0530

    add initial ui

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ai from "../src/assets/ai.png";
+import { RiMic2AiLine } from "react-icons/ri";
+
+const App = () => {
+  return (
+    <div className="w-full h-[100vh] flex flex-col items-center justify-start overflow-hidden p-[10px] gap-[30px]">
+      <img src={ai} className="h-[70%] object-cover" />
+      <span className="bg-gradient-to-r from-pink-400 to-cyan-400 bg-clip-text text-transparent sm:text-lg md:text-xl lg:text-2xl font-semibold">
+        I'm Shifra,Your Advanced Virtual Assistant
+      </span>
+      <button className="bg-gradient-to-r from-pink-500 to-cyan-500 hover:from-pink-400 hover:to-cyan-400 text-white font-semibold shadow-md shadow-cyan-500/40 transition rounded-full px-5 py-2 flex items-center gap-[20px]">
+        click here{" "}
+        <RiMic2AiLine className="w-5 h-5 drop-shadow-[0_0_10px_#ec4899] animate-pulse" />
+      </button>
+    </div>
+  );
+};
+
+export default App;
