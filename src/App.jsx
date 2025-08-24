ff680b3d6df243ee7debe0395a7f215e6fa88780
Manish Shetty <manishshetty827@gmail.com>
2025-08-24 11:38:48 +0530

    Add speach recognition

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { RiMic2AiLine } from "react-icons/ri";
 import { dataContext } from "./context/UserContext";
 
 const App = () => {
-  let {speak}=useContext(dataContext)
+  let { recognition } = useContext(dataContext);
 
   return (
     <div className="w-full h-[100vh] flex flex-col items-center justify-start overflow-hidden p-[10px] gap-[30px]">
@@ -14,7 +14,16 @@ const App = () => {
         I'm Shifra,Your Advanced Virtual Assistant
       </span>
 
-      <button className="bg-gradient-to-r from-pink-500 to-cyan-500 hover:from-pink-400 hover:to-cyan-400 text-white font-semibold shadow-md shadow-cyan-500/40 transition rounded-full px-5 py-2 flex items-center gap-[20px]" onClick={()=>{ speak("Hello Manish")}}>
+      <button
+        className="bg-gradient-to-r from-pink-500 to-cyan-500 hover:from-pink-400 hover:to-cyan-400 text-white font-semibold shadow-md shadow-cyan-500/40 transition rounded-full px-5 py-2 flex items-center gap-[20px]"
+        onClick={() => {
+          if (recognition) {
+            recognition.start();
+          } else {
+            alert("Speech Recognition not supported in this browser");
+          }
+        }}
+      >
         click here
         <RiMic2AiLine className="w-5 h-5 drop-shadow-[0_0_10px_#ec4899] animate-pulse" />
       </button>
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,22 +3,38 @@ import React, { createContext } from "react";
 export const dataContext = createContext();
 
 const UserContext = ({ children }) => {
-    function speak(text){
-        let text_speak=new SpeechSynthesisUtterance(text)
-        text_speak.volume=1;
-        text_speak.rate=1;
-        text_speak.pitch=1;
-        text_speak.lang="hi-US"
-        window.speechSynthesis.speak(text_speak)
+  function speak(text) {
+    let text_speak = new SpeechSynthesisUtterance(text);
+    text_speak.volume = 1;
+    text_speak.rate = 1;
+    text_speak.pitch = 1;
+    text_speak.lang = "hi-US";
+    window.speechSynthesis.speak(text_speak);
+  }
+
+  let speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  let recognition;
+
+  if (speechRecognition) {
+    try {
+      recognition = new speechRecognition();
+      recognition.onresult = (e) => {
+        let currentIndex=e.resultIndex
+        let transcript=e.results[currentIndex][0].transcript
+        console.log(transcript)
+      };
+    } catch (error) {
+      console.log("speechRecognition api not found")
     }
-    let value={
-        speak
-    }
+  }
+
+  let value = {
+    recognition,
+  };
+
   return (
     <div>
-      <dataContext.Provider value={value}>
-        {children}
-      </dataContext.Provider>
+      <dataContext.Provider value={value}>{children}</dataContext.Provider>
     </div>
   );
 };
