7aaf425867cd1c9d4ccf805645a453ce5142f32b
Manish Shetty <manishshetty827@gmail.com>
2025-08-29 18:34:12 +0530

    change gemini model

--- src/gemini.js
+++ src/gemini.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-1.5-flash",
   generationConfig: {
     maxOutputTokens: 30,
     temperature: 0.7, 
